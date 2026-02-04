3d3075819cd8ad1c51e1292db124a97aec2ffff7
babicnik <ratajjachym@gmail.com>
2026-02-04 11:37:21 +0100

    332

---
+++ 3.3.2/script.js
@@ -0,0 +1,20 @@
+let userName = "Tomáš";
+console.log(userName)
+
+userName = "Honzík"
+console.log(userName)
+
+console.log(60+7)
+console.log("6"+"7")
+console.log("A"+"B")
+
+let age = 67;
+console.log(age > 20)
+
+let text = "Text";
+console.log(typeof text)
+
+let a;
+console.log(a)
+let b = null;
+console.log(b)
